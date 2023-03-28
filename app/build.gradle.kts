6480b660f20ff43609a046c1d8b65fb85b49a57a
hyeonseo <s21035@gsm.hs.kr>
2023-03-28 09:29:31 +0900

    :wrench: :: composeOptions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
     buildFeatures {
         compose = true
     }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.4"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8

ef234f12020c50c8e34ce6e6de4c68bc61d69b61
hyeonseo <s21035@gsm.hs.kr>
2023-03-29 14:54:19 +0900

    :sparkles: :: designsystem module

---
+++ designsystem/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id(Dependency.GradlePlugin.ANDROID_LIBRARY)
+    id(Dependency.GradlePlugin.KOTLIN_ANDROID)
+}
+
+android {
+    namespace = "com.daily.designsystem"
+    compileSdk = Versions.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Versions.MIN_SDK
+        targetSdk = Versions.TARGET_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(Dependency.AndroidX.CORE_KTX)
+    implementation(Dependency.AndroidX.APP_COMPAT)
+    implementation(Dependency.Google.MATERIAL)
+    implementation(Dependency.UnitTest.JUNIT)
+    implementation(Dependency.AndroidTest.ANDROID_JUNIT)
+    implementation(Dependency.AndroidTest.ESPRESSO_CORE)
+}

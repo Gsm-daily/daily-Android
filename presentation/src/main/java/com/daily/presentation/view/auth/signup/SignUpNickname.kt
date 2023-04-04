0d28d424cd9fdb7ba6e9425b7e9cb38356bf3c99
hyeonseo <s21035@gsm.hs.kr>
2023-04-04 18:13:29 +0900

    :sparkles: :: SignUpNickname

---
+++ presentation/src/main/java/com/daily/presentation/view/auth/signup/SignUpNickname.kt
@@ -0,0 +1,41 @@
+package com.daily.presentation.view.auth.signup
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.daily.designsystem.component.DailyButton
+import com.daily.designsystem.component.DailyTextField
+import com.daily.designsystem.modifier.dailyClickable
+import com.daily.designsystem.theme.DailyTheme
+import com.daily.designsystem.theme.IcDelete
+import com.daily.presentation.R
+
+@Composable
+fun SignUpNickname(modifier: Modifier = Modifier) {
+    var nickname by remember { mutableStateOf("") }
+
+    Column(modifier = modifier.fillMaxWidth()) {
+        DailyTextField(
+            value = nickname,
+            hint = "예시)고양이귀여워",
+            onValueChanged = { nickname = it },
+            trailingIcon = {
+                if (nickname.isNotEmpty()) {
+                    IcDelete(
+                        contentDescription = "delete",
+                        modifier = modifier.dailyClickable(rippleEnable = false) { nickname = "" },
+                        tint = DailyTheme.color.Neutral40
+                    )
+                }
+            },
+            modifier = modifier.defaultMinSize(minHeight = 24.dp)
+        )
+        Spacer(modifier = modifier.height(154.dp))
+        DailyButton(
+            text = stringResource(R.string.apply),
+            modifier = modifier.fillMaxWidth()
+        ) { }
+    }
+}

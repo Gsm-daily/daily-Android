39999068c8c7e0a8c79f8c372bed9d3d839d11ae
hyeonseo <s21035@gsm.hs.kr>
2023-04-05 20:16:29 +0900

    :sparkles: :: DailyNavHost

---
+++ app/src/main/java/com/daily/navigation/DailyNavHost.kt
@@ -0,0 +1,38 @@
+package com.daily.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.daily.presentation.view.auth.intro.navigation.introScreen
+import com.daily.presentation.view.auth.login.navigation.loginScreen
+import com.daily.presentation.view.auth.login.navigation.navigateToLogin
+import com.daily.presentation.view.auth.signup.navigation.navigateToSignUp
+import com.daily.presentation.view.auth.signup.navigation.signUpScreen
+import com.daily.presentation.view.auth.verification.navigation.navigateToVerification
+import com.daily.presentation.view.auth.verification.navigation.verificationScreen
+
+@Composable
+fun DailyNavHost(
+    navController: NavHostController,
+    startDestination: String
+) {
+    NavHost(navController = navController, startDestination = startDestination) {
+        introScreen(
+            navigateToLogin = { navController.navigateToLogin() },
+            navigateToSignUp = { navController.navigateToSignUp() }
+        )
+        loginScreen(
+            navigateToPrevious = { navController.popBackStack() },
+            navigateToSignUp = { navController.navigateToSignUp() }
+        )
+        signUpScreen(
+            navigateToPrevious = { navController.popBackStack() },
+            navigateToLogin = { navController.navigateToLogin() },
+            navigateToVerification = { navController.navigateToVerification(it) }
+        )
+        verificationScreen(
+            navigateToPrevious = { navController.popBackStack() },
+            navigateToSignUp = { navController.navigateToSignUp(it) }
+        )
+    }
+}

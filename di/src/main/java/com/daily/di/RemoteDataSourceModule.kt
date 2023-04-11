1ba98e3f06c4d11dc97e52e1726fae7b06ea9ba3
hyeonseo <s21035@gsm.hs.kr>
2023-04-11 13:34:41 +0900

    :sparkles: :: RemoteDataSourceModule

---
+++ di/src/main/java/com/daily/di/RemoteDataSourceModule.kt
@@ -0,0 +1,17 @@
+package com.daily.di
+
+import com.daily.data.repository.AuthRepositoryImpl
+import com.daily.domain.repository.AuthRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface RemoteDataSourceModule {
+    @Binds
+    fun bindsAuthDataSource(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
+}

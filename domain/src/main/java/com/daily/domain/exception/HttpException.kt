0378afd66dc0490be21cc86484b23297526a5ff8
hyeonseo <s21035@gsm.hs.kr>
2023-04-11 14:53:51 +0900

    :sparkles: :: HttpException

---
+++ domain/src/main/java/com/daily/domain/exception/HttpException.kt
@@ -0,0 +1,34 @@
+package com.daily.domain.exception
+
+class BadRequestException(
+    override val message: String?
+): RuntimeException()
+
+class UnauthorizedException(
+    override val message: String?
+): RuntimeException()
+
+class ForbiddenException(
+    override val message: String?
+): RuntimeException()
+
+class NotFoundException(
+    override val message: String?
+): RuntimeException()
+
+class ConflictException(
+    override val message: String?
+): RuntimeException()
+
+class ServerException(
+    override val message: String?
+): RuntimeException()
+
+class OtherHttpException(
+    val code: Int?,
+    override val message: String?
+): RuntimeException()
+
+class UnknownException(
+    override val message: String?
+): RuntimeException()

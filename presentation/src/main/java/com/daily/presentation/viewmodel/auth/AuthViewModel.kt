a8ef9720d1b16d0e18de7b7503757ad266a0c77d
hyeonseo <s21035@gsm.hs.kr>
2023-04-13 10:01:26 +0900

    :recycle: :: 기능별 UiState 분리

--- presentation/src/main/java/com/daily/presentation/viewmodel/auth/AuthViewModel.kt
+++ presentation/src/main/java/com/daily/presentation/viewmodel/auth/AuthViewModel.kt
@@ -6,7 +6,7 @@ import com.daily.domain.model.SignUpRequest
 import com.daily.domain.usecase.CheckDuplicateEmailUseCase
 import com.daily.domain.usecase.CheckDuplicateNameUseCase
 import com.daily.domain.usecase.SignUpUseCase
-import com.daily.presentation.viewmodel.util.ExceptionType
+import com.daily.presentation.viewmodel.util.UiState
 import com.daily.presentation.viewmodel.util.exceptionHandling
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -20,8 +20,14 @@ class AuthViewModel @Inject constructor(
     private val checkDuplicateEmailUseCase: CheckDuplicateEmailUseCase,
     private val checkDuplicateNameUseCase: CheckDuplicateNameUseCase
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<AuthUiState>(AuthUiState.Loading)
-    val uiState = _uiState.asStateFlow()
+    private val _signUpUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val signUpUiState = _signUpUiState.asStateFlow()
+
+    private val _duplicateEmailUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val duplicateEmailUiState = _duplicateEmailUiState.asStateFlow()
+
+    private val _duplicateNameUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val duplicateNameUiState = _duplicateNameUiState.asStateFlow()
 
     fun signUp(email: String, password: String, nickname: String) {
         val signUpRequest = SignUpRequest(
@@ -31,28 +37,22 @@ class AuthViewModel @Inject constructor(
         )
         viewModelScope.launch {
             signUpUseCase(signUpRequest)
-                .onSuccess { _uiState.value = AuthUiState.Success }
+                .onSuccess { _signUpUiState.value = UiState.Success }
                 .onFailure {
                     it.exceptionHandling(
-                        badRequestAction = {
-                            _uiState.value = AuthUiState.Error(exception = ExceptionType.BadRequestException)
-                        },
-                        conflictAction = {
-                            _uiState.value = AuthUiState.Error(exception = ExceptionType.ConflictException)
-                        }
+                        badRequestAction = { _signUpUiState.value = UiState.BadRequest },
+                        conflictAction = { _signUpUiState.value = UiState.Conflict }
                     )
                 }
         }
     }
 
-    fun checkDuplicateEmail(email: String) {
+    fun checkDuplicateEmail(email: String, ) {
         viewModelScope.launch {
             checkDuplicateEmailUseCase(email)
-                .onSuccess { _uiState.value = AuthUiState.Success }
+                .onSuccess { _duplicateEmailUiState.value = UiState.Success }
                 .onFailure {
-                    it.exceptionHandling(conflictAction = {
-                        _uiState.value = AuthUiState.Error(exception = ExceptionType.ConflictException)
-                    })
+                    it.exceptionHandling(conflictAction = { _duplicateEmailUiState.value = UiState.Conflict })
                 }
         }
     }
@@ -60,11 +60,9 @@ class AuthViewModel @Inject constructor(
     fun checkDuplicateName(name: String) {
         viewModelScope.launch {
             checkDuplicateNameUseCase(name)
-                .onSuccess { _uiState.value = AuthUiState.Success }
+                .onSuccess { _duplicateNameUiState.value = UiState.Success }
                 .onFailure {
-                    it.exceptionHandling(conflictAction = {
-                        _uiState.value = AuthUiState.Error(exception = ExceptionType.ConflictException)
-                    })
+                    it.exceptionHandling(conflictAction = { _duplicateNameUiState.value = UiState.Conflict })
                 }
         }
     }
--- presentation/src/main/java/com/daily/presentation/viewmodel/email/EmailViewModel.kt
+++ presentation/src/main/java/com/daily/presentation/viewmodel/email/EmailViewModel.kt
@@ -5,33 +5,37 @@ import androidx.lifecycle.viewModelScope
 import com.daily.domain.usecase.SendEmailForPasswordChangeUseCase
 import com.daily.domain.usecase.SendEmailForSignUpUseCase
 import com.daily.domain.usecase.VerifyAuthKeyUseCase
-import com.daily.presentation.viewmodel.util.ExceptionType
+import com.daily.presentation.viewmodel.util.UiState
 import com.daily.presentation.viewmodel.util.exceptionHandling
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class EmailViewModel @Inject constructor(
     private val sendEmailForSignUpUseCase: SendEmailForSignUpUseCase,
     private val verificationCodeUseCase: VerifyAuthKeyUseCase,
     private val sendEmailForPasswordChangeUseCase: SendEmailForPasswordChangeUseCase
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow<EmailUiState>(EmailUiState.Loading)
-    val uiState = _uiState.asStateFlow()
+    private val _signUpEmailUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val signUpEmailUiState = _signUpEmailUiState.asStateFlow()
+
+    private val _verifyUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val verifyUiState = _verifyUiState
+
+    private val _passwordChangeEmailUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val passwordChangeUiState = _passwordChangeEmailUiState.asStateFlow()
 
     fun sendEmailForSignUp(email: String) {
         viewModelScope.launch {
             sendEmailForSignUpUseCase(email)
-                .onSuccess { _uiState.value = EmailUiState.Success }
+                .onSuccess { _signUpEmailUiState.value = UiState.Success }
                 .onFailure {
                     it.exceptionHandling(
-                        badRequestAction = {
-                            _uiState.value = EmailUiState.Error(ExceptionType.BadRequestException)
-                        },
-                        notFoundAction = {
-                            _uiState.value = EmailUiState.Error(ExceptionType.NotFoundException)
-                        }
+                        badRequestAction = { _signUpEmailUiState.value = UiState.BadRequest },
+                        notFoundAction = { _signUpEmailUiState.value = UiState.NotFound }
                     )
                 }
         }
@@ -40,11 +44,9 @@ class EmailViewModel @Inject constructor(
     fun verifyAuthKey(authKey: Int) {
         viewModelScope.launch {
             verificationCodeUseCase(authKey)
-                .onSuccess { _uiState.value = EmailUiState.Success }
+                .onSuccess { _verifyUiState.value = UiState.Success }
                 .onFailure {
-                    it.exceptionHandling(badRequestAction = {
-                        _uiState.value = EmailUiState.Error(ExceptionType.BadRequestException)
-                    })
+                    it.exceptionHandling(badRequestAction = { _verifyUiState.value = UiState.BadRequest })
                 }
         }
     }
@@ -52,15 +54,11 @@ class EmailViewModel @Inject constructor(
     fun sendEmailForPasswordChange(email: String) {
         viewModelScope.launch {
             sendEmailForPasswordChangeUseCase(email)
-                .onSuccess { _uiState.value = EmailUiState.Success }
+                .onSuccess { _passwordChangeEmailUiState.value = UiState.Success }
                 .onFailure {
                     it.exceptionHandling(
-                        badRequestAction = {
-                            _uiState.value = EmailUiState.Error(ExceptionType.BadRequestException)
-                        },
-                        notFoundAction = {
-                            _uiState.value = EmailUiState.Error(ExceptionType.NotFoundException)
-                        }
+                        badRequestAction = { _passwordChangeEmailUiState.value = UiState.BadRequest },
+                        notFoundAction = { _passwordChangeEmailUiState.value = UiState.NotFound }
                     )
                 }
         }

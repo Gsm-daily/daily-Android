49fea1c8618e021f4a84c4c03aba4537af3e2dd8
hyeonseo <s21035@gsm.hs.kr>
2023-04-13 09:54:49 +0900

    :recycle: :: 하나의 UiState에서 상태 관리

--- presentation/src/main/java/com/daily/presentation/viewmodel/auth/AuthUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.daily.presentation.viewmodel.auth
-
-import com.daily.presentation.viewmodel.util.ExceptionType
-
-sealed interface AuthUiState {
-    object Success : AuthUiState
-    data class Error(val exception: ExceptionType) : AuthUiState
-    object Loading : AuthUiState
-}
--- presentation/src/main/java/com/daily/presentation/viewmodel/email/EmailUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.daily.presentation.viewmodel.email
-
-import com.daily.presentation.viewmodel.util.ExceptionType
-
-interface EmailUiState {
-    object Success : EmailUiState
-    data class Error(val exception: ExceptionType) : EmailUiState
-    object Loading : EmailUiState
-}

514a55d749aed34ca4db7512346bf803c176f2de
hyeonseo <s21035@gsm.hs.kr>
2023-05-01 09:46:42 +0900

    :sparkles: :: SignInRequest

--- data/src/main/java/com/daily/data/remote/model/SignInRequestData.kt
+++ data/src/main/java/com/daily/data/remote/model/SignInRequestData.kt
@@ -1,6 +1,14 @@
 package com.daily.data.remote.model
 
+import com.daily.domain.model.SignInRequest
+
 data class SignInRequestData(
     val email: String,
     val password: String
 )
+
+fun SignInRequest.asSignInRequestData() = SignInRequestData(
+    email = email,
+    password = password,
+)
+

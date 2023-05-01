64c425567f55006a005a23f9a6267c9ed8c70fbc
hyeonseo <s21035@gsm.hs.kr>
2023-05-01 09:50:48 +0900

    :sparkles: :: SignInUseCase

---
+++ domain/src/main/java/com/daily/domain/usecase/SignInUseCase.kt
@@ -0,0 +1,11 @@
+package com.daily.domain.usecase
+
+import com.daily.domain.model.SignInRequest
+import com.daily.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class SignInUseCase @Inject constructor(
+    private val repository: AuthRepository
+) {
+    suspend operator fun invoke(signInRequest: SignInRequest) = kotlin.runCatching { repository.signIn(signInRequest) }
+}

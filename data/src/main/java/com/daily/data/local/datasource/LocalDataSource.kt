996b64fbe3b3b92c31776faaaf5f638d1c7005ab
hyeonseo <s21035@gsm.hs.kr>
2023-05-03 10:10:09 +0900

    :sparkles: :: getTokenInfo

--- data/src/main/java/com/daily/data/local/datasource/LocalDataSource.kt
+++ data/src/main/java/com/daily/data/local/datasource/LocalDataSource.kt
@@ -1,9 +1,17 @@
 package com.daily.data.local.datasource
 
+import kotlinx.coroutines.flow.Flow
+
 interface LocalDataSource {
     suspend fun saveToken(
         accessToken: String,
         refreshToken: String,
         accessTokenExpiredAt: String
     )
+
+    suspend fun getAccessToken(): Flow<String>
+
+    suspend fun getRefreshToken(): Flow<String>
+
+    suspend fun getAccessTokenExpiredAt(): Flow<String>
 }

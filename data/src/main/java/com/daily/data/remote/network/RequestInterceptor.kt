3b3aa138291dde40e0313cf0660c373464d40f9a
hyeonseo <s21035@gsm.hs.kr>
2023-05-03 11:23:37 +0900

    :sparkles: :: RequestInterceptor

---
+++ data/src/main/java/com/daily/data/remote/network/RequestInterceptor.kt
@@ -0,0 +1,30 @@
+package com.daily.data.remote.network
+
+import com.daily.data.local.datasource.LocalDataSource
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class RequestInterceptor @Inject constructor(
+    private val localDataSource: LocalDataSource
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val path = request.url().encodedPath()
+        val ignorePath = listOf("/auth", "/account/password", "/email")
+
+        ignorePath.forEach {
+            if (path.contains(it)){
+                return chain.proceed(request)
+            }
+        }
+
+        val accessToken = localDataSource.getAccessToken()
+
+        return chain.proceed(
+            request.newBuilder()
+                .addHeader("Authorization", "Bearer $accessToken")
+                .build()
+        )
+    }
+}

66f52b61ac68234051bbc2fbe0de34f33581f7ab
hyeonseo <s21035@gsm.hs.kr>
2023-05-04 10:38:38 +0900

    :bug: :: 테마 선택시 body를 별도의 data class로 관리

--- data/src/main/java/com/daily/data/remote/network/api/AccountApi.kt
+++ data/src/main/java/com/daily/data/remote/network/api/AccountApi.kt
@@ -1,11 +1,12 @@
 package com.daily.data.remote.network.api
 
+import com.daily.data.remote.model.SelectThemeRequestData
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface AccountApi {
     @POST("/api/v1/account/choice-theme")
     suspend fun choiceTheme(
-        @Body body: String
+        @Body body: SelectThemeRequestData
     )
 }

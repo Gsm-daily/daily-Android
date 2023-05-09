f9a9b487e7fdec0bf5ab27d78ae3285d3f8f8b1b
hyeonseo <s21035@gsm.hs.kr>
2023-05-09 11:04:04 +0900

    :sparkles: :: AccountViewModel changePassword

--- presentation/src/main/java/com/daily/presentation/viewmodel/account/AccountViewModel.kt
+++ presentation/src/main/java/com/daily/presentation/viewmodel/account/AccountViewModel.kt
@@ -2,6 +2,8 @@ package com.daily.presentation.viewmodel.account
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.daily.domain.model.ChangePasswordRequest
+import com.daily.domain.usecase.ChangePasswordUseCase
 import com.daily.domain.usecase.ChoiceThemeUseCase
 import com.daily.presentation.viewmodel.util.UiState
 import com.daily.presentation.viewmodel.util.exceptionHandling
@@ -14,9 +16,13 @@ import javax.inject.Inject
 @HiltViewModel
 class AccountViewModel @Inject constructor(
     private val choiceThemeUseCase: ChoiceThemeUseCase,
+    private val changePasswordUseCase: ChangePasswordUseCase
 ) : ViewModel() {
     private val _choiceThemeUiState = MutableStateFlow<UiState>(UiState.Loading)
     val choiceThemeUiState = _choiceThemeUiState.asStateFlow()
+
+    private val _changePasswordUiState = MutableStateFlow<UiState>(UiState.Loading)
+    val changePasswordUiState = _changePasswordUiState.asStateFlow()
 
     fun choiceTheme(theme: String) {
         viewModelScope.launch {
@@ -32,6 +38,28 @@ class AccountViewModel @Inject constructor(
                         notFoundAction = { _choiceThemeUiState.value = UiState.NotFound },
                         conflictAction = { _choiceThemeUiState.value = UiState.Conflict },
                         unknownAction = { _choiceThemeUiState.value = UiState.Unknown }
+                    )
+                }
+        }
+    }
+
+    fun changePassword(email: String, password: String, newPassword: String) {
+        viewModelScope.launch {
+            changePasswordUseCase(
+                ChangePasswordRequest(
+                    email = email,
+                    password = password,
+                    newPassword = newPassword
+                )
+            )
+                .onSuccess {
+                    _changePasswordUiState.value = UiState.Success
+                }
+                .onFailure {
+                    it.exceptionHandling(
+                        badRequestAction = { _changePasswordUiState.value = UiState.BadRequest },
+                        notFoundAction = { _changePasswordUiState.value = UiState.NotFound },
+                        unknownAction = { _changePasswordUiState.value = UiState.Unknown }
                     )
                 }
         }

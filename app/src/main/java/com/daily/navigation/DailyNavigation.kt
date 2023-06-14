13380880c9054af72cb3532211aa0b7c84f399d2
hyeonseo <s21035@gsm.hs.kr>
2023-06-14 11:16:58 +0900

    :sparkles: :: BottomNavBar로 이동시 백스택에서 경로 삭제

--- app/src/main/java/com/daily/navigation/DailyNavigation.kt
+++ app/src/main/java/com/daily/navigation/DailyNavigation.kt
@@ -3,7 +3,12 @@ package com.daily.navigation
 import androidx.navigation.NavController
 
 fun NavController.navigateToDestination(route: String) {
+    val startRoute = this.graph.startDestinationRoute
+
     this.navigate(route) {
+        startRoute?.let {
+            popUpTo(it)
+        }
         launchSingleTop = true
     }
 }

b13683625b621e6b5407b6d70a5167fe47d38d88
hyeonseo <s21035@gsm.hs.kr>
2023-06-14 10:01:24 +0900

    :lipstick: :: NavBarHeight 수정 (56.dp -> 83.dp)

--- designsystem/src/main/java/com/daily/designsystem/component/navbar/DailyNavBar.kt
+++ designsystem/src/main/java/com/daily/designsystem/component/navbar/DailyNavBar.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.daily.designsystem.theme.DailyTheme
 
-private val NavBarHeight = 56.dp
+private val NavBarHeight = 83.dp
 
 @Composable
 fun DailyNavBar(

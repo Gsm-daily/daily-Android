b5638535899dbbd0405bb1370edc109447c22080
hyeonseo <s21035@gsm.hs.kr>
2023-06-15 10:34:05 +0900

    :sparkles: :: 메인 페이지 viewmodel 로직

--- presentation/src/main/java/com/daily/presentation/view/main/MainScreen.kt
+++ presentation/src/main/java/com/daily/presentation/view/main/MainScreen.kt
@@ -6,29 +6,62 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.daily.designsystem.component.calendar.DailyCalendar
 import com.daily.presentation.R
+import com.daily.presentation.viewmodel.main.MainViewModel
+import com.daily.presentation.viewmodel.util.UiState
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .paint(
-                painterResource(id = R.drawable.bg_summer),
-                contentScale = ContentScale.Crop
-            )
-            .systemBarsPadding()
-    ) {
-        Spacer(modifier = modifier.height(182.dp))
-        DailyCalendar(onClickItem = { })
-        Spacer(modifier = modifier.weight(1f))
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    viewModel: MainViewModel = hiltViewModel()
+) {
+    LaunchedEffect(Unit) {
+        viewModel.getSeason()
+    }
+
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    when (val state = uiState) {
+        is UiState.Success -> {
+            val background = when (state.data!!.season) {
+                "SPRING" -> R.drawable.bg_spring
+                "SUMMER" -> R.drawable.bg_summer
+                "AUTUMN" -> R.drawable.bg_autumn
+                "WINTER" -> R.drawable.bg_winter
+                else -> null
+            }
+
+            background?.let {
+                Column(
+                    modifier = modifier
+                        .fillMaxSize()
+                        .paint(
+                            painter = painterResource(id = background),
+                            contentScale = ContentScale.Crop
+                        )
+                        .systemBarsPadding()
+                ) {
+                    Spacer(modifier = modifier.height(182.dp))
+                    DailyCalendar(onClickItem = { })
+                    Spacer(modifier = modifier.weight(1f))
+                }
+            }
+        }
+        UiState.Unauthorized -> {}
+        UiState.Forbidden -> {}
+        UiState.NotFound -> {}
+        else -> {}
     }
 }
 

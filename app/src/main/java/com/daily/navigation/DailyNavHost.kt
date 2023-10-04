f605f724cd5c573867b02a25e58be509dfb0c8cd
hyeonseo <s21035@gsm.hs.kr>
2023-10-04 20:12:57 +0900

    :fire: :: 카카오 로그인 사용에 따라 자체 로그인 로직 삭제

--- app/src/main/java/com/daily/navigation/DailyNavHost.kt
+++ app/src/main/java/com/daily/navigation/DailyNavHost.kt
@@ -3,19 +3,7 @@ package com.daily.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import com.daily.presentation.view.auth.forgot_password.navigation.forgotPassword
-import com.daily.presentation.view.auth.forgot_password.navigation.forgotPasswordScreen
-import com.daily.presentation.view.auth.forgot_password.navigation.navigateToForgotPassword
-import com.daily.presentation.view.auth.intro.navigation.introScreen
-import com.daily.presentation.view.auth.signin.navigation.signInScreen
-import com.daily.presentation.view.auth.signin.navigation.navigateToSignIn
-import com.daily.presentation.view.auth.select_theme.navigation.navigateToSelectTheme
 import com.daily.presentation.view.auth.select_theme.navigation.selectThemeScreen
-import com.daily.presentation.view.auth.signup.navigation.navigateToSignUp
-import com.daily.presentation.view.auth.signup.navigation.signUpRoute
-import com.daily.presentation.view.auth.signup.navigation.signUpScreen
-import com.daily.presentation.view.auth.verification.navigation.navigateToVerification
-import com.daily.presentation.view.auth.verification.navigation.verificationScreen
 import com.daily.presentation.view.main.navigation.mainScreen
 import com.daily.presentation.view.main.navigation.navigateToMain
 import com.daily.presentation.view.my_theme.navigation.myThemeScreen
@@ -27,46 +15,11 @@ fun DailyNavHost(
     startDestination: String
 ) {
     NavHost(navController = navController, startDestination = startDestination) {
-        introScreen(
-            navigateToSignIn = { navController.navigateToSignIn() },
-            navigateToSignUp = { navController.navigateToSignUp() }
-        )
-        signInScreen(
-            navigateToPrevious = { navController.navigateToPrevious() },
-            navigateToSignUp = { navController.navigateToSignUp() },
-            navigateToForgotPassword = { navController.navigateToForgotPassword() },
-            navigateToMain = { navController.navigateToMain() }
-        )
-        signUpScreen(
-            navigateToPrevious = { navController.navigateToPrevious() },
-            navigateToSignIn = { navController.navigateToSignIn() },
-            navigateToVerification = {
-                navController.navigateToVerification(
-                    email = it,
-                    type = "signup"
-                )
-            },
-            navigateToSelectTheme = { navController.navigateToSelectTheme() }
-        )
-        verificationScreen(
-            navigateToPrevious = { navController.navigateToPrevious() },
-            navigateToNext = {
-                when (navController.previousBackStackEntry?.destination?.route) {
-                    "$signUpRoute?email={email}" -> navController.navigateToSignUp(it)
-                    "$forgotPassword?email={email}" -> navController.navigateToForgotPassword(it)
-                }
-            }
-        )
-        forgotPasswordScreen(
-            navigateToPrevious = { navController.navigateToPrevious() },
-            navigateToSignIn = { navController.navigateToSignIn() },
-            navigateToVerification = {
-                navController.navigateToVerification(
-                    email = it,
-                    type = "password"
-                )
-            }
-        )
+//        TODO: 카카오 로그인 페이지 퍼블리싱 후 navigation 설정
+//        introScreen(
+//            navigateToSignIn = { navController.navigateToSignIn() },
+//            navigateToSignUp = { navController.navigateToSignUp() }
+//        )
         selectThemeScreen(
             navigateToPrevious = { navController.navigateToPrevious() },
             navigateToMain = { navController.navigateToMain() }

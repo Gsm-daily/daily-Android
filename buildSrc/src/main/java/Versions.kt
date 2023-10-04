c592b82b8c822da8499227e9ceadf4ea2bea96d7
hyeonseo <s21035@gsm.hs.kr>
2023-10-04 14:00:36 +0900

    :arrow_up: :: dependencies & sdk version up

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,25 +1,23 @@
 object Versions {
-    const val GRADLE_ANDROID = "7.4.2"
-    const val GRADLE_KOTLIN = "1.8.10"
+    const val GRADLE_ANDROID = "8.1.0"
+    const val GRADLE_KOTLIN = "1.9.10"
     const val KTLINT = "11.6.0"
 
-    const val CORE_KTX = "1.9.0"
+    const val CORE_KTX = "1.12.0"
     const val APP_COMPAT = "1.6.1"
-    const val CORE_SPLASH_SCREEN = "1.0.0"
-    const val COMPOSE_RUNTIME = "1.3.3"
-    const val COMPOSE_MATERIAL3 = "1.0.1"
-    const val ACTIVITY_COMPOSE = "1.6.1"
-    const val COMPOSE_TOOLING = "1.5.0-alpha01"
-    const val COMPOSE_UI = "1.3.3"
-    const val NAVIGATION_COMPOSE = "2.5.3"
-    const val COMPOSE_UI_UTIL = "1.4.1"
-    const val LIFECYCLE = "2.6.1"
+    const val CORE_SPLASH_SCREEN = "1.0.1"
+    const val COMPOSE_RUNTIME = "1.5.2"
+    const val COMPOSE_MATERIAL3 = "1.1.2"
+    const val ACTIVITY_COMPOSE = "1.7.2"
+    const val COMPOSE_UI = "1.5.2"
+    const val NAVIGATION_COMPOSE = "2.7.3"
+    const val LIFECYCLE = "2.6.2"
     const val HILT_NAVIGATION_COMPOSE = "1.0.0"
     const val DATASTORE = "1.0.0"
 
-    const val MATERIAL = "1.8.0"
-    const val HILT = "2.44"
-    const val OKHTTP = "5.0.0-alpha.2"
+    const val MATERIAL = "1.9.0"
+    const val HILT = "2.48"
+    const val OKHTTP = "4.11.0"
 
     const val RETROFIT = "2.9.0"
 
@@ -27,7 +25,7 @@ object Versions {
     const val ANDROID_JUNIT = "1.1.5"
     const val ESPRESSO_CORE = "3.5.1"
 
-    const val COMPILE_SDK = 33
+    const val COMPILE_SDK = 34
     const val MIN_SDK = 26
-    const val TARGET_SDK = 33
+    const val TARGET_SDK = 34
 }

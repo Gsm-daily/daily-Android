176c552812ef60274513f0034bb2b8a720dbd5c4
hyeonseo <s21035@gsm.hs.kr>
2023-10-05 20:32:28 +0900

    :fire: :: 불필요한 navigation logic 삭제

--- app/src/main/java/com/daily/navigation/BackNavigation.kt
+++
@@ -1,9 +0,0 @@
-package com.daily.navigation
-
-import androidx.navigation.NavController
-
-fun NavController.navigateToPrevious() {
-    val previous = this.previousBackStackEntry?.destination?.route
-
-    if (previous != null) this.popBackStack()
-}

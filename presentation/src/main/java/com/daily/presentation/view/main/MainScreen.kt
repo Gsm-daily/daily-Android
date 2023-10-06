61bb84aa34e9e9863d03a4af134977f83e11ac53
hyeonseo <s21035@gsm.hs.kr>
2023-10-06 14:45:14 +0900

    :recycle: :: main screen logic refactoring

--- presentation/src/main/java/com/daily/presentation/view/main/MainScreen.kt
+++ presentation/src/main/java/com/daily/presentation/view/main/MainScreen.kt
@@ -1,65 +1,38 @@
 package com.daily.presentation.view.main
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.daily.designsystem.component.calendar.DailyCalendar
 import com.daily.presentation.R
-import com.daily.presentation.viewmodel.main.MainViewModel
-import com.daily.presentation.viewmodel.util.UiState
 
 @Composable
 fun MainScreen(
     modifier: Modifier = Modifier,
-    viewModel: MainViewModel = hiltViewModel()
 ) {
-    LaunchedEffect(Unit) {
-        viewModel.getSeason()
-    }
-
-    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
-    when (val state = uiState) {
-        is UiState.Success -> {
-            val background = when (state.data!!.season) {
-                "SPRING" -> R.drawable.bg_spring
-                "SUMMER" -> R.drawable.bg_summer
-                "AUTUMN" -> R.drawable.bg_autumn
-                "WINTER" -> R.drawable.bg_winter
-                else -> null
-            }
-
-            background?.let {
-                Column(
-                    modifier = modifier
-                        .fillMaxSize()
-                        .paint(
-                            painter = painterResource(id = R.drawable.bg_autumn),
-                            contentScale = ContentScale.Crop
-                        )
-                        .systemBarsPadding()
-                ) {
-                    Spacer(modifier = modifier.height(182.dp))
-                    DailyCalendar(onDayClick = { })
-                    Spacer(modifier = modifier.weight(1f))
-                }
-            }
-        }
-        is UiState.Error -> TODO()
-        UiState.Loading -> TODO()
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .paint(
+                painter = painterResource(id = R.drawable.bg_autumn), // TODO: 받아온 계절에 맞게 변경
+                contentScale = ContentScale.Crop
+            )
+            .systemBarsPadding(),
+        contentAlignment = Alignment.Center
+    ) {
+        DailyCalendar(
+            modifier = Modifier.padding(horizontal = 14.dp),
+            onDayClick = { /* TODO: 해당 날짜의 Diary로 이동 */ }
+        )
     }
 }
 
